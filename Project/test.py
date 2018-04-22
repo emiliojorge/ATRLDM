68bfab7e77b519e07550a3825ba9510884628e38
johnmoberg <john@labbin.com>
2018-04-22 15:25:43 +0200

    Set up testing environments

--- Project/test.py
+++ Project/test.py
@@ -25,17 +25,34 @@ register(
     kwargs={'map_name': '4x4',
             'is_slippery': True})
 
+register(
+    id='Stochastic-8x8-FrozenLake-v0',
+    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+    kwargs={'map_name': '8x8',
+            'is_slippery': True})
+
 
 def main():
-    env = gym.make('Stochastic-4x4-FrozenLake-v0')
-    blackjack = gym.make('Blackjack-v0')
+    env_names = ['Deterministic-8x8-FrozenLake-v0',
+                  'Stochastic-8x8-FrozenLake-v0',
+                  'Blackjack-v0',
+                  'Roulette-v0',
+                  'NChain-v0']
+    envs = [ gym.make(name) for name in env_names ]
+
     algorithm = base_algorithm.BaseAlgorithm()
-    #score = simulation.simulate(env, algorithm)
-    # Score is a 2D array. Each row corresponds to an environment
-    # And each column correspond to the cumulative reward collected for each trial
-    score = simulation.simulate_multiple_environment([env, blackjack], algorithm)
-    print('Your score is', score)
-    print('Your mean score is ', np.mean(score, axis=1))
+
+    horizon = 1000
+    num_trials = 10
+
+    print(f'Running {len(env_names)} environments for {horizon} timesteps over {num_trials} trials...')
+    score = simulation.simulate_multiple_environment(envs, algorithm, T=horizon, num_trials=num_trials, discount=1)
+    #print('Your score is', score)
+
+    mean_scores = np.mean(score, axis=1)
+    for i, score in enumerate(mean_scores):
+        print(f'Environment: "{env_names[i]}"')
+        print(f'-- Mean score: {score}')
 
 
 if __name__ == '__main__':
