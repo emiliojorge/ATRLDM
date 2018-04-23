06b1b57ab325714427fc391eae503de3a4ff1e91
constant-c <cronrath@chalmers.se>
2018-04-23 15:16:14 +0200

    Basic support for ensemble RL

--- Project/base_algorithm.py
+++ Project/base_algorithm.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from gym import spaces
 
 
@@ -5,11 +7,15 @@ class BaseAlgorithm(object):
     def __init__(self):
         self.action_space = None
 
+        self.agents = []
+
     def reset(self):
         """
         This should reset the algorithm so that it is ready for a new environment
         """
-        pass
+        for a in self.agents:
+            a.reset()
+
 
     def initialize(self, num_states, num_action, discount):
         """
@@ -21,6 +27,9 @@ class BaseAlgorithm(object):
         """
         self.action_space = spaces.Discrete(num_action)
 
+        for a in self.agents:
+            a.initialize(num_states, num_action, discount)
+
     def observe_transition(self, state, action, next_state, reward):
         """
         Observe a new transition: state,action,next_state,reward
@@ -28,7 +37,9 @@ class BaseAlgorithm(object):
         next_state and obtains reward
 
         """
-        pass
+
+        for a in self.agents:
+            a.observe_tranisition(state, action, next_state, reward)
 
     def play(self, state):
         """
@@ -39,6 +50,10 @@ class BaseAlgorithm(object):
         Returns:
             The action to play
         """
+        actions = []
 
-        return self.action_space.sample()
+        for agent in self.agents:
+            action = agent.play(state)
+            actions.append(action)
 
+        return choice(actions)
