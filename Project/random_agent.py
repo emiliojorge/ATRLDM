c2d51a1dfa9aa7f84c9e7e4c3fe10877b4dd84d0
Emilio Jorge <majlon@gmail.com>
2018-04-24 19:11:06 +0200

    Add random agent

---
+++ Project/random_agent.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class RandomAgent(object):
+	"""An agent using random actions"""
+
+	def __init__(self):
+		pass
+
+	def reset(self):
+		"""
+		This should reset the algorithm so that it is ready for a new environment
+		"""
+		pass
+
+	def initialize(self, num_states, num_action, discount):
+		self.num_action = num_action
+
+	def observe_transition(self, state, action, next_state, reward):
+		pass
+
+	def play(self, state):
+		return np.random.randint(self.num_action)
--- Project/test.py
+++ Project/test.py
@@ -1,59 +1,63 @@
 import base_algorithm
+from bayesian_qlearning import Bayesian_Qlearning
+from random_agent import RandomAgent
 import gym
 import numpy as np
 import simulation
 from gym.envs.registration import register
 
 register(
-    id='Deterministic-4x4-FrozenLake-v0',
-    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-    kwargs={'map_name': '4x4',
-            'is_slippery': False})
+	id='Deterministic-4x4-FrozenLake-v0',
+	entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+	kwargs={'map_name': '4x4',
+			'is_slippery': False})
 
 register(
-    id='Deterministic-8x8-FrozenLake-v0',
-    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-    kwargs={'map_name': '8x8',
-            'is_slippery': False})
+	id='Deterministic-8x8-FrozenLake-v0',
+	entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+	kwargs={'map_name': '8x8',
+			'is_slippery': False})
 
 register(
-    id='Stochastic-4x4-FrozenLake-v0',
-    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-    kwargs={'map_name': '4x4',
-            'is_slippery': True})
+	id='Stochastic-4x4-FrozenLake-v0',
+	entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+	kwargs={'map_name': '4x4',
+			'is_slippery': True})
 
 register(
-    id='Stochastic-8x8-FrozenLake-v0',
-    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-    kwargs={'map_name': '8x8',
-            'is_slippery': True})
+	id='Stochastic-8x8-FrozenLake-v0',
+	entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+	kwargs={'map_name': '8x8',
+			'is_slippery': True})
 
 
 def main():
 
-    env_names = ['Deterministic-4x4-FrozenLake-v0',
-                 'Deterministic-8x8-FrozenLake-v0',
-                 'Stochastic-8x8-FrozenLake-v0',
-                 'Blackjack-v0',
-                 'Roulette-v0',
-                 'NChain-v0']
-    envs = [ gym.make(name) for name in env_names ]
+	env_names = ['Deterministic-4x4-FrozenLake-v0',
+				 'Deterministic-8x8-FrozenLake-v0',
+				 'Stochastic-8x8-FrozenLake-v0',
+				 'Blackjack-v0',
+				 'Roulette-v0',
+				 'NChain-v0']
+	envs = [ gym.make(name) for name in env_names ]
 
-    algorithm = base_algorithm.BaseAlgorithm()
-    #algorithm = QAgent(eps_start=0.95, eps_end=0.05, eps_num=1000, learning_rate=lambda n: 1/n)
-    # algorithm = DynaQAgent(planning_steps=50, eps_start=0.95, eps_end=0.05, eps_num=1000, learning_rate=lambda n: 1/n)
+	#algorithm = base_algorithm.BaseAlgorithm()
+	#algorithm = QAgent(eps_start=0.95, eps_end=0.05, eps_num=1000, learning_rate=lambda n: 1/n)
+	# algorithm = DynaQAgent(planning_steps=50, eps_start=0.95, eps_end=0.05, eps_num=1000, learning_rate=lambda n: 1/n)
+	#algorithm = Bayesian_Qlearning(action_selection="random", update_method="mom")
+	#algorithm = RandomAgent()
 
-    horizon = 5000
-    num_trials = 10
+	horizon = 20000
+	num_trials = 3
 
-    print(f'Running {len(env_names)} environments for {horizon} timesteps over {num_trials} trials...')
-    scores = simulation.simulate_multiple_environment(envs, algorithm, T=horizon, num_trials=num_trials, discount=1)
-    #print('Your score is', scores)
+	print(f'Running {len(env_names)} environments for {horizon} timesteps over {num_trials} trials...')
+	scores = simulation.simulate_multiple_environment(envs, algorithm, T=horizon, num_trials=num_trials, discount=1)
+	#print('Your score is', scores)
 
-    mean_scores = np.mean(scores, axis=1)
-    for i, score in enumerate(mean_scores):
-        print(f'Environment: "{env_names[i]}"')
-        print(f'-- Mean reward: {score}')
+	mean_scores = np.mean(scores, axis=1)
+	for i, score in enumerate(mean_scores):
+		print(f'Environment: "{env_names[i]}"')
+		print(f'-- Mean reward: {score}')
 
 if __name__ == '__main__':
-    main()
+	main()
