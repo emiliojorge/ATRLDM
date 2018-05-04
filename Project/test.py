282d203aa40379cfeb1c76d6867c60de41090521
constant-c <cronrath@chalmers.se>
2018-05-04 14:04:42 +0200

    save results to csv

--- Project/test.py
+++ Project/test.py
@@ -9,85 +9,89 @@ from random_agent import RandomAgent
 from util import EpsilonGreedy
 
 register(
-	id='Deterministic-4x4-FrozenLake-v0',
-	entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-	kwargs={'map_name': '4x4',
-			'is_slippery': False})
+    id='Deterministic-4x4-FrozenLake-v0',
+    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+    kwargs={'map_name': '4x4',
+            'is_slippery': False})
 
 register(
-	id='Deterministic-8x8-FrozenLake-v0',
-	entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-	kwargs={'map_name': '8x8',
-			'is_slippery': False})
+    id='Deterministic-8x8-FrozenLake-v0',
+    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+    kwargs={'map_name': '8x8',
+            'is_slippery': False})
 
 register(
-	id='Stochastic-4x4-FrozenLake-v0',
-	entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-	kwargs={'map_name': '4x4',
-			'is_slippery': True})
+    id='Stochastic-4x4-FrozenLake-v0',
+    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+    kwargs={'map_name': '4x4',
+            'is_slippery': True})
 
 register(
-	id='Stochastic-8x8-FrozenLake-v0',
-	entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-	kwargs={'map_name': '8x8',
-			'is_slippery': True})
+    id='Stochastic-8x8-FrozenLake-v0',
+    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+    kwargs={'map_name': '8x8',
+            'is_slippery': True})
 
 
 def main():
+    env_names = ['Deterministic-4x4-FrozenLake-v0',
+                 'Deterministic-8x8-FrozenLake-v0',
+                 'Stochastic-8x8-FrozenLake-v0',
+                 'Stochastic-4x4-FrozenLake-v0',
+                 'Blackjack-v0',
+                 'Roulette-v0',
+                 'NChain-v0']
 
-	env_names = ['Deterministic-4x4-FrozenLake-v0',
-				 'Deterministic-8x8-FrozenLake-v0',
-				 'Stochastic-8x8-FrozenLake-v0',
-				 'Stochastic-4x4-FrozenLake-v0',
-				 'Blackjack-v0',
-				 'Roulette-v0',
-				 'NChain-v0']
+    rng = np.random.RandomState(25)
 
-	rng = np.random.RandomState(25)
+    repeat_environments = False
+    compare_random = True
 
-	repeat_environments = False
-	compare_random = True
     save_results = True
 
-	if repeat_environments == True:
-		env_names = rng.choice(env_names, size=12, replace=True)
+    if repeat_environments == True:
+        env_names = rng.choice(env_names, size=12, replace=True)
 
-	envs = [ gym.make(name) for name in env_names ]
+    envs = [gym.make(name) for name in env_names]
 
-	algorithm = BaseAlgorithm(exploration=True, explorer=EpsilonGreedy(start=1.0, end=0.01, steps=5000), use_database=True)
-	#algorithm = ZapQAgent()
+    algorithm = BaseAlgorithm(exploration=True, explorer=EpsilonGreedy(start=1.0, end=0.01, steps=5000),
+                              use_database=True)
+    # algorithm = ZapQAgent()
     # algorithm = QAgent(exploration=True, explorer=EpsilonGreedy(start=0.95, end=0.05, steps=1000))#eps_start=0.95, eps_end=0.05, eps_num=1000, learning_rate=lambda n: 1/n)
-	#algorithm = DynaQAgent()#planning_steps=50, eps_start=0.95, eps_end=0.05, eps_num=1000, learning_rate=lambda n: 1/n)
-	#algorithm = Bayesian_Qlearning()#action_selection="q-sampling", update_method="mom")
-	#algorithm = Speedy_Qlearning()
-	#algorithm = MeanAgent()
+    # algorithm = DynaQAgent()#planning_steps=50, eps_start=0.95, eps_end=0.05, eps_num=1000, learning_rate=lambda n: 1/n)
+    # algorithm = Bayesian_Qlearning()#action_selection="q-sampling", update_method="mom")
+    # algorithm = Speedy_Qlearning()
+    # algorithm = MeanAgent()
 
-	random_algorithm = RandomAgent()
+    random_algorithm = RandomAgent()
 
     horizon = 2000
-	num_trials = 5
+    num_trials = 5
 
-	print(f'Running {len(env_names)} environments for {horizon} timesteps over {num_trials} trials...')
-	start = time.time()
-	scores = simulation.simulate_multiple_environment(envs, algorithm, T=horizon, num_trials=num_trials, discount=1)
-	print(f'{algorithm.algorithm} took {time.time()-start} seconds')
-	#print('Your score is', scores)
-	mean_scores = np.mean(scores, axis=1)
-	std_scores = np.std(scores, axis=1)
+    print(f'Running {len(env_names)} environments for {horizon} timesteps over {num_trials} trials...')
+    start = time.time()
+    scores = simulation.simulate_multiple_environment(envs, algorithm, T=horizon, num_trials=num_trials, discount=1)
+    print(f'{algorithm.algorithm} took {time.time()-start} seconds')
+    # print('Your score is', scores)
+    mean_scores = np.mean(scores, axis=1)
+    std_scores = np.std(scores, axis=1)
 
-	if compare_random == True:
-		random_scores = simulation.simulate_multiple_environment(envs, random_algorithm, T=horizon, num_trials=num_trials, discount=1)
-		mean_random = np.mean(random_scores, axis=1)
-		std_random = np.std(random_scores, axis=1)
-		for i, (score, rand_score, score_std, rand_std) in enumerate(zip(mean_scores, mean_random, std_scores, std_random)):
-			print(f'Environment: "{env_names[i]}"')
-			print(f'-- Mean reward: {score} -- Std: {score_std}')
-			print(f'-- Random reward: {rand_score} -- Std: {rand_std}')
+    if compare_random == True:
+        random_scores = simulation.simulate_multiple_environment(envs, random_algorithm, T=horizon,
+                                                                 num_trials=num_trials,
+                                                                 discount=1)
+        mean_random = np.mean(random_scores, axis=1)
+        std_random = np.std(random_scores, axis=1)
+        for i, (score, rand_score, score_std, rand_std) in enumerate(
+                zip(mean_scores, mean_random, std_scores, std_random)):
+            print(f'Environment: "{env_names[i]}"')
+            print(f'-- Mean reward: {score} -- Std: {score_std}')
+            print(f'-- Random reward: {rand_score} -- Std: {rand_std}')
 
-	else:
-		for i, (score,score_std) in enumerate(zip(mean_scores, std_scores)):
-			print(f'Environment: "{env_names[i]}"')
-			print(f'-- Mean reward: {score} -- Var: {score_std}')
+    else:
+        for i, (score, score_std) in enumerate(zip(mean_scores, std_scores)):
+            print(f'Environment: "{env_names[i]}"')
+            print(f'-- Mean reward: {score} -- Var: {score_std}')
 
     if save_results:
         with open('results.csv', 'w') as file:
@@ -98,4 +102,4 @@ def main():
         return env_names, scores
 
 if __name__ == '__main__':
-	main()
+    main()
